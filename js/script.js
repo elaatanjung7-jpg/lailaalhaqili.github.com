13f6556a2f55a5111b76db03646d566da293547b
elaatanjung7-jpg <elaatanjung7@gmail.com>
2026-02-24 08:22:33 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
+function register() {
+    localStorage.setItem("firstName", document.getElementById("regFirstName").value);
+    localStorage.setItem("lastName", document.getElementById("regLastName").value);
+    localStorage.setItem("password", document.getElementById("regPassword").value);
 
+    alert("Pendaftaran berhasil!");
+    window.location.href = "login.html";
+}
+
+function login() {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const password = document.getElementById("password").value;
+
+    if (
+        firstName === localStorage.getItem("firstName") &&
+        lastName === localStorage.getItem("lastName") &&
+        password === localStorage.getItem("password")
+    ) {
+        window.location.href = "home.html";
+    } else {
+        alert("Data salah atau belum daftar!");
+    }
+}
